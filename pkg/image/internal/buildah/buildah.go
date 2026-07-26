63c94365de8e80f3826a1ab2a593ef23c6951252
Donglin-Wang <wdl448@yahoo.com>
2026-07-26 14:37:00 -0400

    Buildah version bump

--- pkg/image/internal/buildah/buildah.go
+++ pkg/image/internal/buildah/buildah.go
@@ -33,7 +33,7 @@ const (
 	defaultVersion     = "v0.1.0-beta.4"
 	defaultAMD64URL    = "https://github.com/donglin-wang/chamber/releases/download/v0.1.0-beta.4/buildah-worker-linux-amd64"
 	defaultAMD64SHA256 = "649dea3351658956bbca0a635baca721968f4cb37b20db8a344d817ac7415bcd"
-	defaultARM64URL    = "https://github.com/donglin-wang/chamber/releases/download/v0.1.0-beta.4/buildah-worker-linux-arm64"
+	defaultARM64URL    = "https://github.com/donglin-wang/chamber/releases/download/v0.1.0-beta.5/buildah-worker-linux-arm64"
 	defaultARM64SHA256 = "d9f75975799c0ed2d176a89405518a1760a7e183e4269876c0920aaedd791355"
 )
 
